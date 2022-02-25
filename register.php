a700cd051a062321c9b949431be471ebbee6d986
Danila <Danila>
2022-02-25 18:27:11 +0300

    С доработками валидации

--- register.php
+++ register.php
@@ -9,31 +9,37 @@
     <link rel="stylesheet" href="css/main.css" />
     <script>
         function validate(){
+            if (document.getElementById("pass1").value!=document.getElementById("pass2").value){
+                document.getElementById("SubButton").innerText="Пароли не совпадают";
+
+                sovpass = false;
+                // document.getElementById("SubButton").disabled=true;
+            }
+            else{
+                document.getElementById("SubButton").innerText="Submit";
+
+                sovpass = true;
+                // document.getElementById("SubButton").disabled=false;
+            }
+
             if(document.getElementById("pass1").value==""){
                 document.getElementById("alertpdw").innerText="Пароль не может быть пустым";
                 document.getElementById("alertpdw").style.color="red";
                 document.getElementById("alertpdw").style.display="";
-                document.getElementById("SubButton").disabled=true;
+
+                nepass = false;
+                // document.getElementById("SubButton").disabled=true;
             }
             else{
                 document.getElementById("alertpdw").innerText="";
                 document.getElementById("alertpdw").style.display="none";
-                document.getElementById("SubButton").disabled=false;
+
+                nepass = true;
+                // document.getElementById("SubButton").disabled=false;
             }
 
-            console.log("Часть 1")
-
-            if (document.getElementById("pass1").value!=document.getElementById("pass2").value){
-                document.getElementById("SubButton").innerText="Пароли не совпадают";
-                document.getElementById("SubButton").disabled=true;
-            }
-            else{
-                document.getElementById("SubButton").innerText="Submit";
-                document.getElementById("SubButton").disabled=false;
-            }
-
-            console.log("Часть 2")
-           
+            document.getElementById("SubButton").disabled= !(nepass && sovpass);
+            console.log(nepass && sovpass)
         }
     </script>
 </head>
